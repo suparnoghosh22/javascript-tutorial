823dfc0c24dfbcc4a3961ad0b48ee4da3fff1516
Suparno <suparno22official@gmail.com>
2025-12-10 23:51:24 +0530

    Updated

--- Datatypes/main.js
+++ Datatypes/main.js
@@ -13,8 +13,36 @@ console.log(typeof 23); // typeof value -> To determine the datatype of a value
 console.log(typeof undefined); // It is a undefined type
 console.log(typeof null); // In JS null is defined as an object type
 
+// Primitive Datatype
+// 7 types (Call by value): Number, String, Boolean, null, undefined, Symbol, Bigint
 
+// Reference type (Non Primitive)
+// Array, Objects, Functions
 
+//Symbol
+const id = Symbol("123")
+const anotherId = Symbol("123")
 
+console.log(id === anotherId)
+
+// Big Int
+const bigNumber = 25837507253009235n 
+
+// Arrays
+let heroes = ["Superman","Spider-man","Batman"]
+
+// Objects
+let myDetails = {
+    name: "Suparno",
+    age: 23,
+    address: ["Kolkata","West Bengal"]
+}
+
+// Function
+const myFunc = function() {
+    console.log("Hello World")
+}
+
+myFunc()
 
  
