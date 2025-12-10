fb26c758451f7f69ae627f64b9f2100481fd0557
Suparno <suparno22official@gmail.com>
2025-12-10 21:29:15 +0530

    Operations in JS

---
+++ Operations/main.js
@@ -0,0 +1,35 @@
+// Arithmetic operations
+let value = 3
+let negValue = -value
+console.log(value + 3)
+console.log(negValue)
+
+// +, -, *, /, **, %
+
+let str1 = "suparno"
+let str2 = " ghosh"
+let str3 = 12
+console.log(str1 + str2) // String concatenation
+console.log(str1 + str3)
+
+console.log("1" + 2) // 12 String type
+console.log("1" + 2 + 2)  // 122 String type
+console.log(1 + 2 + "2"); // 32 String type 
+
+console.log(+true) // 1
+// console.log(true+); Error
+
+console.log(+""); // 0
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 1
+
+console.log(3 - "4"); // -1 --> It treats as subtraction operation
+
+
+
+
+
+
+
