b44c870ca65910332d4ab53b77ac11c274e8784b
Suparno <suparno22official@gmail.com>
2025-12-10 15:14:05 +0530

    Dataypes in JS

---
+++ Datatypes/main.js
@@ -0,0 +1,17 @@
+"use strict"; // treat all JS code as newer version
+
+ // number
+ // bigint
+ // string
+ // boolean => true/false
+ // null => empty value(Standalone value)
+ // undefined => No value assigned
+ // symbol => to define unique values
+ // object
+
+console.log(typeof 23); // typeof value -> To determine the datatype of a value
+console.log(typeof undefined); // It is a undefined type
+console.log(typeof null); // In JS null is defined as an object type
+
+
+ 
